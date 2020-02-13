ea691bdea68b03d8aa9529610342ca9af4c070fc
Caleb Bolton <crbolton@ucdavis.edu>
2020-02-12 16:33:06 -0800

    Added critical sections

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -7,7 +7,7 @@
 
 #include <stdio.h>
 
-typedef struct semaphore {
+typdef stuct semaphore {
 	size_t count;
 	queue_t blockedQueue;
 } semaphore;
@@ -20,7 +20,7 @@ sem_t sem_create(size_t count)
 	queue_t newQueue = queue_create();
 	if (newQueue == NULL) return NULL;
 	newSem->blockedQueue = newQueue;
-	printf("sem_create\n");
+	//printf("sem_create\n");
 	return newSem;
 }
 
@@ -30,50 +30,60 @@ int sem_destroy(sem_t sem)
 	// Can't destroy semaphore if it still contains blocked threads.
 	if (queue_length(sem->blockedQueue) > 0) return -1;
 	if (queue_destroy(sem->blockedQueue) < 0) return -1;
-	printf("sem_destroy\n");
+	//printf("sem_destroy\n");
+    printf("freeing sem\n");
 	free(sem);
 	return 0;
 }
 
 int sem_down(sem_t sem)
 {
+	enter_critical_section();
+    printf("sem_down\n");
 	if (sem == NULL) return -1;
 	// No resources left, so wait in queue
 	if (sem->count <= 0) {
 		pthread_t* tid = malloc(sizeof(pthread_t));
 		*tid = pthread_self();
 		if (queue_enqueue(sem->blockedQueue, (void*) tid) < 0) return -1;
+		printf("Blocked thread %d\n", (int) *tid);
 		if (thread_block() < 0) return -1;
-		printf("sem_down: count == 0, blocked thread%d\n", (int) *tid);
+		//printf("sem_down: count == 0, blocked thread %d\n", (int) *tid);
 	}
 	// There are available resources, so decrement 
 	else {
 		sem->count--;
-		printf("sem_down: count > 0, consume resource\n");
+		//printf("sem_down: count > 0, consume resource\n");
 	}
+	exit_critical_section();
 	return 0;
 }
 
 int sem_up(sem_t sem)
 {
+	enter_critical_section();
 	if (sem == NULL) return -1;
 	printf("sem_up\n");
 	// There are blocked threads, so unblock oldest one
 	if (queue_length(sem->blockedQueue) > 0) {
-		//printf("\tUnblocking oldest thread\n");
+		//printf("Unblocking oldest thread\n");
 		pthread_t* unblockedTid;
 		if (queue_dequeue(sem->blockedQueue, (void**) &unblockedTid) < 0) return -1;
 		if (thread_unblock(*unblockedTid) < 0) return -1;
+        printf("unblocked thread %d\n", (int) *unblockedTid);
 		free(unblockedTid);
 	}
 	sem->count++;
+	exit_critical_section();
 	return 0;
 }
 
 int sem_getvalue(sem_t sem, int *sval)
 {
+	enter_critical_section();
 	if (sem == NULL || sval == NULL) return -1;
 	*sval = sem->count;
+	exit_critical_section();
 	return 0;
 }
 
