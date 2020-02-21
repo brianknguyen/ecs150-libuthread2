239d1d2cd948f489b3be515fa2dc3d875382b1ce
Caleb Bolton <crbolton@ucdavis.edu>
2020-02-20 23:20:34 -0800

    Minor changes

--- libuthread/tps.c
+++ libuthread/tps.c
@@ -12,7 +12,6 @@
 #include "thread.h"
 #include "tps.h"
 
-/* TODO: Phase 2 */
 
 typedef struct Page {
 	char* addr;
@@ -26,7 +25,8 @@ typedef struct TPS {
 
 queue_t tpsQueue = NULL;
 
-static int find_TPS_From_Page(void *data, void* arg)
+// Find TPS based on its page's starting address
+static int findTpsFromPageAddr(void *data, void* arg)
 {
 	TPS* a = (TPS*)data;
 	void* match = arg;
@@ -37,7 +37,7 @@ static int find_TPS_From_Page(void *data, void* arg)
 }
 
 // Find TPS based on tid
-static int find_TPS(void *data, void *arg)
+static int findTpsFromTid(void *data, void *arg)
 {
     TPS *a = (TPS*)data;
     int match = (int)(long) arg;
@@ -56,7 +56,7 @@ static void segv_handler(int sig, siginfo_t *si, __attribute__((unused)) void *c
     void* p_fault = (void*)((uintptr_t)si->si_addr & ~(TPS_SIZE - 1));
 
 	TPS* foundTPS = NULL;
-	queue_iterate(tpsQueue, find_TPS_From_Page, (void*) p_fault, (void**) &foundTPS);
+	queue_iterate(tpsQueue, findTpsFromPageAddr, (void*) p_fault, (void**) &foundTPS);
 
     if (foundTPS != NULL)
         /* Printf the following error message */
@@ -99,7 +99,7 @@ int tps_create(void)
 	// Attempt to find thread's current TPS
 	TPS* foundTPS = NULL;
 	enter_critical_section();
-	if (queue_iterate(tpsQueue, find_TPS, (void*) pthread_self(), (void**) &foundTPS) < 0) {
+	if (queue_iterate(tpsQueue, findTpsFromTid, (void*) pthread_self(), (void**) &foundTPS) < 0) {
 		exit_critical_section();
 		return -1;
 	}
@@ -110,7 +110,7 @@ int tps_create(void)
 	}
 	exit_critical_section();
 
-	// Allocate page for TPS
+	// Allocate and initialize page for TPS
 	void* pageAddr = mmap(NULL, TPS_SIZE, PROT_NONE, MAP_PRIVATE | MAP_ANON, -1, 0);
 	if (pageAddr == (void*) -1) return -1;
 
@@ -118,6 +118,7 @@ int tps_create(void)
 	newPage->addr = pageAddr;
 	newPage->count = 1;
 
+	// Allocate and initialize new TPS
 	TPS* newTPS = malloc(sizeof(TPS));
 	newTPS->page = newPage;
 	newTPS->tid = pthread_self();
@@ -135,27 +136,32 @@ int tps_create(void)
 
 int tps_destroy(void)
 {
+	// Find the thread's TPS
 	TPS* foundTPS = NULL;
 	enter_critical_section();
-	if (queue_iterate(tpsQueue, find_TPS, (void*) pthread_self(), (void**) &foundTPS) < 0) {
+	if (queue_iterate(tpsQueue, findTpsFromTid, (void*) pthread_self(), (void**) &foundTPS) < 0) {
 		exit_critical_section();
 		return -1;
 	}
 
+	// If the thread has no TPS, error
 	if(foundTPS == NULL) {
 		exit_critical_section();
 		return -1;
 	}
 
+	// If the thread's TPS is not the only one using the page it refers to, the just decrement the count on the page
 	if (foundTPS->page->count > 1) {
 		foundTPS->page->count--;
 	}
+	// If the thread's TPS is the only one using the page it refers to, then we can delete the page
 	else {
 		munmap(foundTPS->page->addr, TPS_SIZE);
 		free(foundTPS->page);
 		free(foundTPS);
 	}
 
+	// Delete the TPS either way
 	queue_delete(tpsQueue, foundTPS);
 	exit_critical_section();
 
@@ -167,10 +173,10 @@ int tps_read(size_t offset, size_t length, void *buffer)
 	// Buffer can't be NULL
 	if (buffer == NULL) return -1;
 
-	// Find TPS
+	// Find thread's TPS to read from
 	TPS* foundTPS = NULL;
 	enter_critical_section();
-	if (queue_iterate(tpsQueue, find_TPS, (void*) pthread_self(), (void**) &foundTPS) < 0) {
+	if (queue_iterate(tpsQueue, findTpsFromTid, (void*) pthread_self(), (void**) &foundTPS) < 0) {
 		exit_critical_section();
 		return -1;
 	}
@@ -181,6 +187,7 @@ int tps_read(size_t offset, size_t length, void *buffer)
 		return -1;
 	}
 
+	// Allow reading, read, then disable reading
 	mprotect(foundTPS->page->addr, length, PROT_READ);
 	memcpy(buffer, foundTPS->page->addr + offset, length);
 	mprotect(foundTPS->page->addr, length, PROT_NONE);
@@ -196,7 +203,7 @@ int tps_write(size_t offset, size_t length, void *buffer)
 	// Find TPS
 	TPS* foundTPS = NULL;
 	enter_critical_section();
-	if (queue_iterate(tpsQueue, find_TPS, (void*) pthread_self(), (void**) &foundTPS) < 0) {
+	if (queue_iterate(tpsQueue, findTpsFromTid, (void*) pthread_self(), (void**) &foundTPS) < 0) {
 		exit_critical_section();
 		return -1;
 	}
@@ -207,6 +214,9 @@ int tps_write(size_t offset, size_t length, void *buffer)
 		return -1;
 	}
 	
+	// If there are multiple TPSs that are using the page this thread's TPS is associated with,
+	// we have to make a new page, copy the data from the old page to the new one and then decrement
+	// the count of the old page
 	if (foundTPS->page->count > 1) {
 		// Allocate page for TPS
 		void* pageAddr = mmap(NULL, TPS_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
@@ -214,12 +224,15 @@ int tps_write(size_t offset, size_t length, void *buffer)
 		Page* newPage = malloc(sizeof(Page));
 		newPage->addr = pageAddr;
 		newPage->count = 1;
+		// Allow reading and writing
 		mprotect(foundTPS->page->addr, length, PROT_WRITE | PROT_READ);
+		// Copy data from old page to the new one
 		memcpy(newPage->addr, foundTPS->page->addr, TPS_SIZE);
 		foundTPS->page->count--;
 		foundTPS->page = newPage;
 	}
 	
+	// Write new data to the new page
 	mprotect(foundTPS->page->addr, length, PROT_WRITE);
 	memcpy(foundTPS->page->addr + offset, buffer, length);
 	mprotect(foundTPS->page->addr, length, PROT_NONE);
@@ -233,7 +246,7 @@ int tps_clone(pthread_t tid)
 	// Attempt to find thread's current TPS
 	TPS* curTPS = NULL;
 	enter_critical_section();
-	if (queue_iterate(tpsQueue, find_TPS, (void*) pthread_self(), (void**) &curTPS) < 0) {
+	if (queue_iterate(tpsQueue, findTpsFromTid, (void*) pthread_self(), (void**) &curTPS) < 0) {
 		exit_critical_section();
 		return -1;
 	}
@@ -244,17 +257,21 @@ int tps_clone(pthread_t tid)
 	}
 	exit_critical_section();
 
-	// Find TPS
+	// Find TPS with given tid
 	TPS* foundTPS = NULL;
 	enter_critical_section();
-	if (queue_iterate(tpsQueue, find_TPS, (void*) tid, (void**) &foundTPS) < 0) {
+	if (queue_iterate(tpsQueue, findTpsFromTid, (void*) tid, (void**) &foundTPS) < 0) {
 		return -1;
 	}
 	
+	// If the TPS doesn't exist, error
 	if (foundTPS == NULL) {
 		exit_critical_section();
 		return -1;
 	}
+
+	// Allocate a new TPS whose page is the same one as the TPS with the given tid
+	// and increment the page's count
 	TPS* newTPS = malloc(sizeof(TPS));
 	newTPS->page = foundTPS->page;
 	newTPS->tid = pthread_self();
